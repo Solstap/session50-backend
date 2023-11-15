5de3ae0f2ac908612cbd480847cbd9b04105b3ec
Roxana Mihaila <emmyta93@gmail.com>
2023-11-15 19:38:46 +0000

    worked on package.json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const server = require("./server.js"); // 2
 
-const { PORT = 3000 } = process.env; // 4
+const { PORT = 3000, NODE_ENV=development } = process.env; // 4 WITHOUT NODE_ENV 
+
+console.log(NODE_ENV); // 12 WITH NODE ENV
 
 server.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}`);
--- middleware.js
+++ middleware.js
@@ -2,4 +2,17 @@ const express = require("express"); //6
 
 module.exports = function (app) {
   app.use(express.static("public"));
+
+
+  const cars = [{name:"Nissan"}, {name: "Toyota"}]; //9
+
+  app.get('/api/v1/cars', (req, res) =>{
+    res.status(200).json(cars); // 10
+  })
+  app.post('/api/v1/cars', (req, res) => {
+    const newCar = req.body 
+    cars.push(newCar) 
+    req.sendStatus(201) // 11
+  })
+
 }; // 7
