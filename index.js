df6ecd889be3717a8be38f2d6949eeb58b879c9a
Roxana Mihaila <emmyta93@gmail.com>
2023-11-15 20:00:08 +0000

    amended

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const server = require("./server.js"); // 2
 
-const { PORT = 3000, NODE_ENV=development } = process.env; // 4 WITHOUT NODE_ENV 
-
-console.log(NODE_ENV); // 12 WITH NODE ENV
+const { PORT = 3000, NODE_ENV = "development" } = process.env; // 4
 
 server.listen(PORT, () => {
   console.log(`Server running on: http://localhost:${PORT}`);
-});
+}); // 12
